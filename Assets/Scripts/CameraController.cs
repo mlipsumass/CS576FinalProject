f335af005da416c5ac22f04cec89161347649a11
Matt Lips <mlips@umass.edu>
2023-12-06 17:36:18 -0500

    Added Mars scene with martian terrain and a simple camera controller to look around

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public float rotationSpeed = 5f;
+	public float movementSpeed = 5f;
+	public float verticalSpeed = 5f;
+
+	void Start()
+	{
+		// Hide the mouse cursor at the beginning
+		Cursor.visible = false;
+	}
+
+	void Update()
+	{
+		// Hide the mouse cursor when clicking
+		if (Input.GetMouseButtonDown(0))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		// Mouse rotation
+		float mouseX = Input.GetAxis("Mouse X");
+
+		transform.Rotate(Vector3.up * mouseX * rotationSpeed);
+
+		// Keyboard movement
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+		Vector3 moveAmount = moveDirection * movementSpeed * Time.deltaTime;
+
+		transform.Translate(moveAmount);
+
+		// Arrow key vertical movement
+		float verticalTranslation = 0;
+
+		if (!Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space))
+		{
+			verticalTranslation = verticalSpeed * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space))
+		{
+			verticalTranslation = -verticalSpeed * Time.deltaTime;
+		}
+
+		// Move the camera up or down based on space key input
+		transform.Translate(Vector3.up * verticalTranslation);
+	}
+}
